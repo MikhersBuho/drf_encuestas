5686fa309932cc54a5888b7830a38e04998d87ab
Miguel Moreno <miguel.morenou@gnilat.com>
2023-05-08 12:18:59 -0500

    Variables de entorno añadidas 'de DJango y postgres'

--- api_encuestas/models.py
+++ api_encuestas/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+# class usuario(models.Model):
+#     nombre= models.CharField(max_length=100)
+#     apellido= models.CharField(max_length=100)
+#     user_name= models.CharField(max_length=100)
+#     email= models.CharField(max_length=255)
+#     password= models.CharField(max_length=255)
+#     is_active= models.BooleanField(default=False)
+#     create_at= models.DateTimeField(auto_now_add=True)
--- drf_encuestas/settings.py
+++ drf_encuestas/settings.py
@@ -9,23 +9,26 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+#environ init
+env = environ.Env()
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v4#hht5o%w114sg3_4(k91c#ord6c^fnks8t^#rf7pz^%-wn-1'
+SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = tuple(env.list('ALLOWED_HOSTS', default=[]))
 
 
 # Application definition
@@ -76,9 +79,13 @@ WSGI_APPLICATION = 'drf_encuestas.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env.str('DB_NAME'),
+        'USER': env.str('DB_USER'),
+        'PASSWORD': env.str('DB_PASSWORD'),
+        'HOST': env.str('DB_HOST'),
+        'PORT': env.str('DB_PORT'),
     }
 }
 
